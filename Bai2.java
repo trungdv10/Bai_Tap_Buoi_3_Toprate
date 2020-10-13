989b91523d479773dcf9d0f458239b5fea0f992c
Trung Duong Van <trunga1k16byt@gmail.com>
2020-10-14 00:53:58 +0700

    add Bai2.java

---
+++ Bai2.java
@@ -0,0 +1,12 @@
+package com.vnpt.media.ottplus.model;
+
+public class Bai2 {
+
+	/* Lý do sử dụng hàm getter, setter:
+	 * 1, Đóng gói các hành động liên quan đến việc get hoặc set thuộc tính, điều này cho phép thêm chức năng bổ sung
+	 * 		như xác thực dễ dàng hơn sau này
+	 * 2, Tăng cường bảo mật dữ liệu 
+	 * 
+	 * */
+	
+}
