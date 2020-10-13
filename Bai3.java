abf3c44d0fe7deddd2e0460c823faec7d9679c40
Trung Duong Van <trunga1k16byt@gmail.com>
2020-10-14 00:54:23 +0700

    add Bai3.java

---
+++ Bai3.java
@@ -0,0 +1,15 @@
+package com.vnpt.media.ottplus.model;
+
+public class Bai3 {
+
+	/* Chúng ta có thể khai báo một constructor là private, ta có thể sử dụng constructor private này 
+	 * 	trong Singleton Design Pattern.
+	 * Điều kiện cho private constructor:
+	 * + 1 private constructor không cho phép 1 lớp có lớp con
+	 * + không cho phép tạo một đối tượng bên ngoài class
+	 * + Nếu tất cả các phương thức là static, chúng ta có thể sử dụng 1 private constructor
+	 * + Nếu cố gắng extends 1 class có private constructor, lỗi compile time error sẽ xảy ra
+	 * 
+	 * */
+	
+}
